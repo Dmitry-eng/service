2fd3fa130401fb5fc7f2b3c4aa511db30a44e709
ASSA <11dima1996@gmail.com>
2023-09-11 22:31:37 +0300

    fixed

--- src/main/java/com/service/service/AccountService.java
+++ src/main/java/com/service/service/AccountService.java
@@ -3,10 +3,6 @@ package com.service.service;
 import com.service.dto.account.AccountCreate;
 import com.service.dto.account.AccountInfo;
 import com.service.entity.AccountEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
-
-import java.security.Security;
 import java.util.List;
 
 public interface AccountService {
